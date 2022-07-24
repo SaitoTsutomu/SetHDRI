7a7399ad946dc8b8d2198e62f350608ff43e0cdf
Saito Tsutomu <tsutomu7@hotmail.co.jp>
2022-07-24 20:21:11 +0900

    upd

--- __init__.py
+++ __init__.py
@@ -1,5 +1,3 @@
-import webbrowser
-
 import bpy
 
 bl_info = {
@@ -16,12 +14,34 @@ bl_info = {
 }
 
 
+def set_3dview_shading(shading_type):
+    for area in bpy.context.screen.areas:
+        if area.type == "VIEW_3D":
+            for space in area.spaces:
+                if space.type == "VIEW_3D":
+                    space.shading.type = shading_type
+
+
 class CSH_OT_set_hdri(bpy.types.Operator):
     bl_idname = "object.set_hdri"
     bl_label = "Set HDRI"
     bl_description = "Set the world image."
 
+    filepath: bpy.props.StringProperty()  # type: ignore # noqa
+
+    def invoke(self, context, event):
+        context.window_manager.fileselect_add(self)
+        return {"RUNNING_MODAL"}
+
     def execute(self, context):
+        bpy.context.scene.world.use_nodes = True
+        nt = bpy.context.scene.world.node_tree
+        et = nt.nodes.get("Environment Texture")
+        if not et:
+            et = nt.nodes.new("ShaderNodeTexEnvironment")
+            nt.links.new(et.outputs[0], nt.nodes["Background"].inputs[0])
+        et.image = bpy.data.images.load(filepath=self.filepath)
+        set_3dview_shading("RENDERED")
         return {"FINISHED"}
 
 
